9527aa57462b5be1a33d71ee37dbb227437ebc25
LGMarques9963 <lorran9963@gmail.com>
2022-03-04 15:22:58 -0300

    LAB07

--- Lorran-7-LAB07.c
+++ Lorran-7-LAB07.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
-#define tamanhoVetor = 20
+#define tamanhoVetor 20
+
 int main(){
-    int numeros[tamanhoVetor], indice, numRepetido, contador;
+    
+    int numeros[tamanhoVetor], indice, numRepetido, contador, i;
     indice=0;
     contador = 0;
+
     do{
         printf("Digite um numero inteiro: ");
-        scanf("%d", numeros[indice]);
-        printf('\n');
+        scanf("%d", &numeros[indice]);
+        printf("\n");
         indice++;
     }while(indice<tamanhoVetor);
 
     printf("\nDigite o numero que deseja verificar: ");
-    scanf("%d", numRepetido);
+    scanf("%d", &numRepetido);
 
-    for(int i=0;i<tamanhoVetor;i++){
-        if(numRepetido == numeros[tamanhoVetor]) contador += 1;
+    for(i=0;i<tamanhoVetor;i++){
+        if(numRepetido == numeros[i]) {
+                contador += 1;
+        }
     }
 
     printf("\nO numero %d se repete %d vezes", numRepetido, contador);
