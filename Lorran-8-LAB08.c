d2056e2841b184eea74407b795c9e050f39c0430
LGMarques9963 <lorran9963@gmail.com>
2022-03-08 16:34:52 -0300

    Update Lorran-8-LAB08.c

--- Lorran-8-LAB08.c
+++ Lorran-8-LAB08.c
@@ -8,6 +8,8 @@
 int main(){
     char prefixo[8];
     int sufixo1, sufixo2;
+    char endereco_ip[16];
+    char sufixo[8];
 
     printf("Entre com o prefixo do endereco de IP: \n");
     gets(prefixo);
@@ -16,7 +18,12 @@ int main(){
     sufixo1 = rand() % INTERVALO_SUPERIOR;
     sufixo2 = rand() % INTERVALO_SUPERIOR;
 
+    printf("%s %d %d", prefixo,sufixo1,sufixo2);
+    sprintf(sufixo,".%d.%d",sufixo1,sufixo2);
+    strcat(endereco_ip,prefixo);
+    strcat(endereco_ip,sufixo);
 
+    printf("\nO endereco IP gerado eh:\t %s", endereco_ip);
 
 
 
