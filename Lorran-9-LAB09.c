608b2196639f5124332295103d3a4ff565a8c082
LGMarques9963 <lorran9963@gmail.com>
2022-03-15 19:45:36 -0300

    LAB09

---
+++ Lorran-9-LAB09.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#define COLUNAS_MATRIZ 3
+#define LINHAS_MATRIZ 3
+
+int main(){
+    int matriz[LINHAS_MATRIZ][COLUNAS_MATRIZ];
+	int i,j,aux;
+    for(i=0;i<LINHAS_MATRIZ;i++){
+        for(j=0;j<COLUNAS_MATRIZ;j++){
+            scanf("%i", &matriz[i][j]);
+        }
+    }
+	
+	
+	printf("\nMATRIZ ORIGINAL\n");
+    for(i=0;i<LINHAS_MATRIZ;i++){
+        for(j=0;j<COLUNAS_MATRIZ;j++){
+            printf("%i \t", matriz[i][j]);
+        }
+        printf("\n");
+	}
+	
+	printf("\nVALORES MAXIMOS DE CADA COLUNA\n");
+	for(i=0;i<COLUNAS_MATRIZ;i++){
+		aux = 0;
+		for(j=0;j<LINHAS_MATRIZ;j++){
+			if(matriz[j][i]>aux) aux=matriz[j][i]; 
+		}
+		printf("[%i]: %i \t", i,aux);
+	}
+	printf("\n");
+
+
+    return 0;
+}
